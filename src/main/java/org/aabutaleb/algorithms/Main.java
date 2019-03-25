25c2afb590273e85b3adbdb45445ce7cf5f46ed5
Amin Abu-taleb <abta.amin@gmail.com>
2019-03-25 20:03:00 +0000

    Finished Mergesort

--- src/main/java/org/aabutaleb/algorithms/Main.java
+++ src/main/java/org/aabutaleb/algorithms/Main.java
@@ -1,11 +1,11 @@
 package org.aabutaleb.algorithms;
 
-import org.aabutaleb.algorithms.sort.insertion.Insertion;
+import org.aabutaleb.algorithms.sort.merge.Merge;
 import org.aabutaleb.algorithms.util.ResultChecker;
 
 public class Main {
     public static void main(String[] args) {
-        Insertion algorithm = Insertion.build();
+        Merge algorithm = Merge.build();
 
         algorithm.run();
         algorithm.print();
--- src/main/java/org/aabutaleb/algorithms/sort/merge/Merge.java
+++ src/main/java/org/aabutaleb/algorithms/sort/merge/Merge.java
@@ -4,10 +4,14 @@ import org.aabutaleb.algorithms.sort.Sort;
 import org.aabutaleb.algorithms.util.DataLoader;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class Merge implements Sort {
-    private int[] data;
+    private List<Integer> data;
 
     private Merge(){}
 
@@ -28,22 +32,43 @@ public class Merge implements Sort {
      */
     @Override
     public void run(){
-        for (int i = 0; i < data.length; i++){
-            var min = i;
-            for (int j = i+1; j < data.length; j++) {
-                if (data[j] < data[min]) min = j;
-            }
-            exch(data, i, min);
+        data = sort(data);
+
+    }
+
+    private List<Integer> sort(List<Integer> list) {
+        if (list.size() > 1) {
+            List<Integer> l1 = sort(list.subList(0, list.size() / 2));
+            List<Integer> l2 = sort(list.subList((list.size() / 2), list.size()));
+            return merge(l1, l2);
+        }
+        else{
+            return list;
         }
     }
 
+    private List<Integer> merge(List<Integer> l1, List<Integer> l2) {
+        List<Integer> result = new ArrayList();
+        for (int i=0, j=0; (i<l1.size() || j<l2.size()); ){
+            if (j==l2.size() || i<l1.size() && l1.get(i) < l2.get(j)) {
+                result.add(l1.get(i++));
+            }
+            else if (i==l1.size() || j<l2.size() && l2.get(j) < l1.get(i)){
+                result.add(l2.get(j++));
+            }
+        }
+
+        return result;
+    }
+
+
     @Override
     public int[] getData() {
-        return data;
+        return data.stream().mapToInt(x -> x).toArray();
     }
 
     @Override
     public void setData(int[] data) {
-        this.data = data;
+        this.data = Arrays.stream(data).boxed().collect(Collectors.toList());
     }
 }
